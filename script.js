21b7e47bfcd00bbc06e8d170799ece45cc6952cc
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-04 20:04:00 +0800

    Create conditionals
    if-else statements are used to check for user inputs.
    Code will only proceed to next step if the userInput is correct.
    Will not continue if cancelled, left blank, or wrong input.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // Asks the user for Username
 let userInput = prompt("Username");
 
+// * Conditionals to check for the user input and renders respective outputs
+// Runs if user input is ADMIN (case sensitive)
+if (userInput == "ADMIN") {
+    alert("Welcome");
+}
+// Runs if user pressed cancel or left the input empty
+else if (userInput == null || userInput == false) {
+    alert("Cancelled");
+}
+// Runs if user has entered the wrong input != ADMIN
+else {
+    alert("I don't know you!");
+}
