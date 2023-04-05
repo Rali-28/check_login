84fab83d5b7a691dd6dfbc20838cb53cd40ff7cc
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-05 10:11:47 +0800

    Rearrange if-else order
    Code will check first if user press cancelled or empty input before proceeding to check for correct input.

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 let userInput = prompt("Username");
 
 // * Conditionals to check for the user input and renders respective outputs
-// Runs if user input is ADMIN (case sensitive)
-if (userInput == "ADMIN") {
-    alert("Welcome");
-}
+
 // Runs if user pressed cancel or left the input empty
-else if (userInput == null || userInput == false) {
+if (userInput == null || userInput == false) {
     alert("Cancelled");
 }
+
+// Runs if user input is ADMIN (case sensitive)
+else if (userInput == "ADMIN") {
+    alert("Welcome");
+}
+
 // Runs if user has entered the wrong input != ADMIN
 else {
     alert("I don't know you!");
